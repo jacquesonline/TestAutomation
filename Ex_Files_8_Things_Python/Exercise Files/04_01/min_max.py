578a9a57071e121a74fc94afe046df5991d11a71
jacquesonline <j.c.steenkamp10@gamil.com>
2025-02-27 09:33:51 +1100

    commit

--- Ex_Files_8_Things_Python/Exercise Files/04_01/min_max.py
+++ Ex_Files_8_Things_Python/Exercise Files/04_01/min_max.py
@@ -1,3 +1,6 @@
 countries = ['Netherlands', 'Nigeria', 'Jordan', 'Nepal', 'Niger', 'Japan']
-population = [17_500_000, 198_000_000, 10_000_000, 30_000_000, 24_000_000, 128_000_000]
+population = [17_500_000, 198_000_000, 10_000_000,
+              30_000_000, 24_000_000, 128_000_000]
 
+cats = ('Al', 'Tina', 'Sasha', 'Pierre')
+print(f'{max(cats)}')
